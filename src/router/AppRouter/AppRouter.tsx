9d6de6cac59d34d8204d688cbb6d91637a3d1be9
ruslan khilkovets <ruslanxilkovec@gmail.com>
2024-07-10 14:43:42 +0300

    Added base routing

---
+++ src/router/AppRouter/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import routes from '../routes';
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            {
+                routes.map(route => {
+                    return (
+                        <Route Component={ route.component } path={ route.path }/>
+                    )
+                })
+            }
+        </Routes>
+    );
+};
+
+export default AppRouter;
---
+++ src/router/routes.ts
@@ -0,0 +1,14 @@
+import CatalogPage from "../pages/CatalogPage";
+import CheckoutPage from "../pages/CheckoutPage";
+import ItemPage from "../pages/ItemPage";
+import MainPage from "../pages/MainPage";
+import PATHS from "./paths";
+
+export const routes = [
+    { path: PATHS.CATALOG, component: CatalogPage },
+    { path: PATHS.MAIN_PAGE, component: MainPage },
+    { path: PATHS.CHECKOUT, component: CheckoutPage },
+    { path: PATHS.ITEM, component: ItemPage },
+];
+
+export default routes;
