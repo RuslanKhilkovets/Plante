18765183926a2960235c45cd8989c3469cde07ca
ruslan khilkovets <ruslanxilkovec@gmail.com>
2024-07-25 11:09:38 +0300

    adaptive fixes

--- src/components/FilteredItems/FilteredItems.tsx
+++ src/components/FilteredItems/FilteredItems.tsx
@@ -4,12 +4,15 @@ import { TProductFullData } from '../../types/IProductItem';
 
 import cl from "./FilteredItems.module.scss";
 import ProductItem from '../Products/ProductItem/ProductItem';
+import clsx from 'clsx';
 
 
 const FilteredItems: FC<{ items: TProductFullData[] }> = ({ items = [] }) => {
 
     return (
-        <div className={cl["filtered-items"]}>
+        <div className={clsx(["filtered-items"] , {
+            [cl["empty"]]: items.length === 0
+        })}>
             {
                 items.length !== 0 ? items?.map(item => {
                     return (
