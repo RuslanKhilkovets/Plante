889239f18b6b54912e96a3e59c43077b6987747d
ruslan khilkovets <ruslanxilkovec@gmail.com>
2024-07-25 11:20:06 +0300

    Fixed empty items

--- src/components/FilteredItems/FilteredItems.tsx
+++ src/components/FilteredItems/FilteredItems.tsx
@@ -1,28 +1,36 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import clsx from 'clsx';
 
 import { TProductFullData } from '../../types/IProductItem';
 
 import cl from "./FilteredItems.module.scss";
 import ProductItem from '../Products/ProductItem/ProductItem';
-import clsx from 'clsx';
 
 
 const FilteredItems: FC<{ items: TProductFullData[] }> = ({ items = [] }) => {
+    const [content, setContent] = useState<any>();
 
-    return (
-        <div className={clsx(["filtered-items"] , {
-            [cl["empty"]]: items.length === 0
-        })}>
-            {
-                items.length !== 0 ? items?.map(item => {
-                    return (
-                        <ProductItem product={item} key={item.id + item.title}/>
-                    )
-                }) : <>Немає даних</>
+    const renderMainContent = () => {
+        return (
+            <div className={clsx(cl["filtered-items"] , {
+                [cl["empty"]]: items.length === 0
+            })}>
+                {
+                    items.length !== 0 ? items?.map(item => {
+                        return (
+                            <ProductItem product={item} key={item.id + item.title}/>
+                        )
+                    }) : <p className={cl["empty__text"]}>Немає даних</p>
+    
+                }
+            </div>
+        )
+    }
+    useEffect(() => {
+        setContent(renderMainContent());
+    }, [items])
 
-            }
-        </div>
-    );
+    return content;
 };
 
 export default FilteredItems;
